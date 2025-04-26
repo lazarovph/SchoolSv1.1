6d1b9900259d9f8e2228f2380ce8230cc19c3b7f
lazarovph <90379778+lazarovph@users.noreply.github.com>
2025-04-26 13:25:10 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,10 @@ from flask import render_template, redirect, url_for, request, flash
 from app import db
 from app.models import User, Task, Solution
 from flask_login import login_user, login_required, current_user, logout_user
-from app import app
+from app import create_app
+
+app = create_app()
+
 
 @app.route('/')
 @login_required
